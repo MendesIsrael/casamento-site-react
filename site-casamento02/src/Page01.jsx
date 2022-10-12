fae9a6ead45f9f9d975ce9f8d8f3d91bf2c04c78
Israel Mendes <101142371+MendesIsrael@users.noreply.github.com>
2022-10-12 10:27:47 -0300

    Inserindo conteúdo

--- site-casamento02/src/Page01.jsx
+++ site-casamento02/src/Page01.jsx
@@ -1,14 +1,25 @@
 export function Page01() {
     return (
-        <article> 
-        <p>
-            Criamos esse site para compartilhar com vocês os detalhes da organização do nosso casamento. Estamos muito felizes e contamos com a presença de todos no nosso grande dia!
-            
-            Ah, é importante também confirmar sua presença com a nossa assessoria. Aguarde, eles entrarão em contato com você. Somente com eles que a sua presença será confirmada.
-            
-            Para nos presentear, escolha qualquer item da nossa Lista de Casamento. Fiquem à vontade!
-            
-        </p>
-    </article>
+        <div>
+            <div> 
+                <p>
+                    gjfhjCriamos esse site para compartilhar com vocês os detalhes da organização do nosso casamento. Estamos muito felizes e contamos com a presença de todos no nosso grande dia!
+                    
+                    Ah, é importante também confirmar sua presença com a nossa assessoria. Aguarde, eles entrarão em contato com você. Somente com eles que a sua presença será confirmada.
+                    
+                    Para nos presentear, escolha qualquer item da nossa Lista de Casamento. Fiquem à vontade!
+                    
+                </p>
+            </div>
+            <div>
+                <h1>
+                    NOSSA HISTÓRIA
+                </h1>
+                <p>
+                    Essa história não nasceu para ser simples. Ele flamenguista, ela vascaína. Ele de humanas, ela de biológicas. Ele do céu, ela do mar. Indivíduos sinérgicos, de mundos distintos, mas complementares. Dentre tantas diferenças, uma única singularidade fez com que os destinos se cruzassem: a paixão pela bola pesada e as quatro linhas. Marcação que começou em quadra e nunca mais parou. Bolada certeira no coração. E não se sabe como e nem o porquê, mas apesar dos impedimentos marcados pela vida, algo dizia que não adiantava resistir, não tínhamos chance. Era ele, o amor, quem estava conduzindo a bola e ganhando o jogo.
+                    Hoje, juntos, por um ideal maior, jogamos no mesmo time e vivemos o início de um sonho que antes de ser nosso, era do Senhor. Desse modo, no dia 08 de julho vamos marcar um golaço e contamos com vocês para fazer do nosso grande dia tão especial quanto a final da Champions League.
+                </p>
+            </div>
+        </div>    
     )
 }
